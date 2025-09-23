60533a8d5ed0b38b0c0ed3991c16c229ebf84789
nikol <nikol@DESKTOP-93NIME5>
2025-09-23 13:16:49 +0700

    Добавьте файлы проекта.

---
+++ ImageCypherLib.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCypherLib
+{
+    public class ImageCypherLib
+    {
+        //Decode
+        public static BitArray DecypherImage(Bitmap image, bool filterZeroBytes)
+        {
+            List<bool> bits = new List<bool>();
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    bits.AddRange(GetBitsFromPixel(image.GetPixel(j, i)));
+                }
+            }
+            if (filterZeroBytes)
+            bits = FilterZeroBytes(bits);
+            return new BitArray(bits.ToArray());
+        }
+
+        private static List<bool> FilterZeroBytes(List<bool> bits)
+        {
+            List<bool> ret = new List<bool>();
+            for (int i = 0; i < bits.Count; i += 8)
+            {
+                int sum = 0;
+                for (int j = 0; j < 8; j++) if (i + j < bits.Count) sum += bits[i + j] ? 1 : 0;
+                if (sum != 0) for (int j = 0; j < 8; j++) ret.Add(bits[i + j]);
+                else break;
+            }
+            return ret;
+        }
+
+        private static bool[] GetBitsFromPixel(Color pixel)
+        {
+            return new bool[3] { new BitArray(new byte[] { pixel.R })[0], new BitArray(new byte[] { pixel.G })[0], new BitArray(new byte[] { pixel.B })[0] };
+        }
+
+        //Encode
+        public static Bitmap CypherImage(Bitmap image, BitArray messageBits)
+        {
+            if (image.Width * image.Height * 3 < messageBits.Length) throw new Exception("Message lengtn bigger then image capacity");
+            Color[] imagePixels = GetPixelsFromImage(image);
+            int count = 0;
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    image.SetPixel(j, i, CypherPixel(image.GetPixel(j, i), messageBits, count));
+                    count += 3;
+                }
+            }
+            Console.WriteLine();
+            return image;
+        }
+
+        private static Color CypherPixel(Color pixel, BitArray message, int counter)
+        {
+            BitArray pixelR = new BitArray(new byte[] { pixel.R });
+            BitArray pixelG = new BitArray(new byte[] { pixel.G });
+            BitArray pixelB = new BitArray(new byte[] { pixel.B });
+            if (counter < message.Length)
+                pixelR[0] = message[counter];
+            else
+                pixelR[0] = false;
+
+            if (counter + 1 < message.Length)
+                pixelG[0] = message[counter + 1];
+            else
+                pixelG[0] = false;
+
+            if (counter + 2 < message.Length)
+                pixelB[0] = message[counter + 2];
+            else
+                pixelB[0] = false;
+            return Color.FromArgb(pixel.A, BitArrayToByteArray(pixelR)[0], BitArrayToByteArray(pixelG)[0], BitArrayToByteArray(pixelB)[0]);
+        }
+
+        public static byte[] BitArrayToByteArray(BitArray bits)
+        {
+            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+            return ret;
+        }
+
+        private static Color[] GetPixelsFromImage(Bitmap image)
+        {
+            List<Color> ret = new List<Color>();
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    ret.Add(image.GetPixel(j, i));
+                }
+            }
+            return ret.ToArray();
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ImageCypherLib;
+using static ImageCypherLib.ImageCypherLib;
+
+namespace ImageCypher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                string message = Console.ReadLine();
+                Bitmap bitmap = new Bitmap(Image.FromFile("img.jpg"));
+                BitArray messageBits = new BitArray(Encoding.GetEncoding(1251).GetBytes(message));
+                DebugBitArray(messageBits);
+                Console.WriteLine("Cyphering...");
+                bitmap = CypherImage(bitmap, messageBits);
+                Console.WriteLine("Decyphering...");
+                messageBits = DecypherImage(bitmap, true);
+                DebugBitArray(messageBits);
+                Console.WriteLine(EncodeBitArrayMessage(messageBits));
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static string EncodeBitArrayMessage(BitArray message)
+        {
+            int bitcounter = 0;
+            string ret = "";
+            List<bool> bitsBuffer = new List<bool>();
+            foreach (bool bit in message)
+            {
+                bitcounter++;
+                bitsBuffer.Add(bit);
+                if (bitcounter >= 8)
+                {
+                    bitcounter = 0;
+                    BitArray letter = new BitArray(8);
+                    for (int i = 0; i < 8; i++)
+                    {
+                        letter[i] = bitsBuffer[0];
+                        bitsBuffer.RemoveAt(0);
+                    }
+                    ret += Encoding.GetEncoding(1251).GetString(BitArrayToByteArray(letter));
+                }
+            }
+            return ret;
+        }
+
+        private static void DebugBitArray(BitArray messageBits)
+        {
+            foreach (bool b in messageBits)
+            {
+                Console.Write(b ? 1 : 0);
+            }
+            Console.WriteLine();
+        }
+    }
+}
