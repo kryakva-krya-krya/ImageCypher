948005002fb06a055ba4e7a0ddebe7fea71f6a7b
Vladislav <NeProstoyVariantik@gmail.com>
2025-09-23 17:27:47 +0700

    fixed program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
-using ImageCypherLib;
 using static ImageCypherLib.Cypher;
 
 namespace ImageCypher
